b09c82c3d174c448d1e0e6826f4ccc951c6cc572
assly <blue20will@gmail.com>
2018-09-18 16:33:32 +0900

    add: Create breadcrumb with its helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # render breadcrumbs
+  # always print 'Home'
+  #
+  # argument example:
+  #   breadcrumb({ text: 'Home', href: root_path }, { text: 'Home', href: root_path })
+  def breadcrumb(*links)
+    render 'layouts/breadcrumb', links: [{ text: 'Home', href: root_path }] + links
+  end
 end
