46042f9fd39aec14118811266ae884839f6d3d94
assly <blue20will@gmail.com>
2018-09-18 15:15:13 +0900

    add: Add home controller with root_path"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'home#index'
   resources :patients, shallow: true do
     resources :orders, shallow: true do
       resources :inspections
