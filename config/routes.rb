edaa3962ea4f39a10f15e592597e469688539ccf
assly <blue20will@gmail.com>
2018-09-18 14:56:02 +0900

    add: Create header and sidebar and modify routings

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'inspections/index'
-  get 'inspections/show'
-  get 'inspections/new'
-  get 'inspections/edit'
-  get 'orders/index'
-  get 'orders/new'
-  get 'orders/show'
-  get 'orders/edit'
-  get 'patients/index'
-  get 'patients/show'
-  get 'patients/edit'
   resources :patients, shallow: true do
     resources :orders, shallow: true do
       resources :inspections
