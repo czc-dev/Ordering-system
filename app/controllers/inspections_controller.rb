44b28c9321d31e106351e220692a3088ee82ea26
assly <blue20will@gmail.com>
2018-09-19 22:11:42 +0900

    add: Create to edit inspection method with back button and modify h1.title of order/inspection

--- app/controllers/inspections_controller.rb
+++ app/controllers/inspections_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class InspectionsController < ApplicationController
-  before_action :set_order, only: :index
+  before_action :set_order, only: %i[index create new]
 
   def index
     @inspections = @order.inspections
@@ -11,16 +11,30 @@ class InspectionsController < ApplicationController
     @inspection = Inspection.find_by(id: params[:id])
   end
 
+  def edit
+    @inspection = Inspection.find_by(id: params[:id])
+    @order = @inspection.order
+  end
+
   def create
   end
 
   def update
+    @inspection = Inspection.find_by(id: params[:id])
+    @inspection.update!(update_params)
+
+    flash[:success] = '更新しました。'
+    redirect_to order_inspections_url(@inspection.order)
   end
 
   def destroy
   end
 
   private
+
+  def update_params
+    params.require(:inspection).permit(:status_id, :urgent, :cancelled)
+  end
 
   def set_order
     @order = Order.find_by(id: params[:order_id])
