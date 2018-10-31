710fd321701c8684cb59b77f350e9ad04ba9c7a1
assly <blue20will@gmail.com>
2018-10-31 21:23:21 +0900

    add: Generate Employees controller and fix not to generate controller spec

--- web/config/application.rb
+++ web/config/application.rb
@@ -33,7 +33,7 @@ module Ordering
     config.generators do |g|
       g.test_framework(:rspec,
         fixtures: true,
-        controller_specs: true,
+        controller_specs: false,
         view_specs: false,
         helper_specs: false,
         routing_specs: false)
