af30074277fea685d573007fef3e785eef17b539
assly <blue20will@gmail.com>
2018-11-01 11:23:10 +0900

    fix: Fix to create fixture; use password pair instead of its digest(wrong)

--- web/spec/factories/employees.rb
+++ web/spec/factories/employees.rb
@@ -2,6 +2,9 @@ FactoryBot.define do
   factory :employee do
     fullname { Faker::Name.name }
     username { Faker::Internet.username }
-    password_digest { 'fa5473530e4d1a5a1e1eb53d2fedb10c' } # 'employee'
+    password { 'employee' }
+    password_confirmation { 'employee' }
+  end
+
   end
 end
