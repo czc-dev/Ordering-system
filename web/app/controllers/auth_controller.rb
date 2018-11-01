9c7856de8fcecce96c03c80049bb33272c643060
assly <blue20will@gmail.com>
2018-11-01 12:05:55 +0900

    add: Create Auth controller and improve its spec

--- web/app/controllers/auth_controller.rb
+++ web/app/controllers/auth_controller.rb
@@ -1,12 +1,29 @@
 # frozen_string_literal: true
 
 class AuthController < ApplicationController
-  def login
-  end
+  def login; end
 
   def create
+    employee = Employee.find_by(username: params[:username])
+    # '&.' is 'safe navigation'
+    if employee&.authenticate(params[:password])
+      session[:current_employee_id] = employee.id
+      flash[:success] = 'ログインしました。'
+      redirect_to root_path
+    else
+      flash.now[:warning] = '正しく入力してください。'
+      render 'login'
+    end
   end
 
   def destroy
+    if session[:current_employee_id]
+      reset_session
+      flash[:success] = 'ログアウトしました。'
+      redirect_to root_path
+    else
+      flash[:warning] = 'ログインしてください。'
+      redirect_to login_path
+    end
   end
 end
--- web/spec/requests/auths_spec.rb
+++ web/spec/requests/auths_spec.rb
@@ -30,16 +30,21 @@ RSpec.describe "Auths", type: :request do
 
   describe 'DELETE /logout' do
     context 'when employee is logged in' do
-      before { delete logout_path }
-
-      it { should redirect_to(root_path) }
+      let(:valid_employee) { { username: username, password: 'password' } }
+      before do
+        post login_path, params: valid_employee
+        delete logout_path
+      end
 
       it 'removes session' do
-        expect(session[:current_employee_id]).to be_empty
+        expect(session[:current_employee_id]).to be_nil
       end
+
+      it { should redirect_to(root_path) }
     end
 
     context 'when employee is not logged in' do
+      before { delete logout_path }
       it { should redirect_to(login_path) }
     end
   end
