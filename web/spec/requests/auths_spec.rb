15b89f8b27019ced804237c8160301d134066922
arsley <blue20will@gmail.com>
2018-11-16 22:05:14 +0900

    add: Improve Auth's spec; assert redirection without logged in

--- web/spec/requests/auths_spec.rb
+++ web/spec/requests/auths_spec.rb
@@ -48,4 +48,81 @@ RSpec.describe "Auths", type: :request do
       it { should redirect_to(login_path) }
     end
   end
+
+  describe 'GET any endpoints without logged in' do
+    let(:employee_id) { create(:employee).id }
+    let(:patient_id) { create(:patient).id }
+    let(:order_id) { create(:order).id }
+    let(:inspection_id) { create(:inspection).id }
+
+    context 'GET /' do
+      before { get root_path }
+      it { should redirect_to(login_path) }
+    end
+
+    context 'GET /new/order' do
+      before { get new_order_path }
+      it { should redirect_to(login_path) }
+    end
+
+    context 'GET /employees' do
+      before { get employees_path }
+      it { should redirect_to(login_path) }
+    end
+
+    context 'GET /employees/new' do
+      before { get new_employee_path }
+      it { should redirect_to(login_path) }
+    end
+
+    context 'GET /employees/:id/edit' do
+      before { get edit_employee_path(employee_id) }
+      it { should redirect_to(login_path) }
+    end
+
+    context 'GET /patients' do
+      before { get patients_path }
+      it { should redirect_to(login_path) }
+    end
+
+    context 'GET /patients/new' do
+      before { get new_patient_path }
+      it { should redirect_to(login_path) }
+    end
+
+    context 'GET /patients/:id/edit' do
+      before { get edit_patient_path(patient_id) }
+      it { should redirect_to(login_path) }
+    end
+
+    context 'GET /patients/:patient_id/orders' do
+      before { get patient_orders_path(patient_id) }
+      it { should redirect_to(login_path) }
+    end
+
+    context 'GET /patients/:patient_id/orders/new' do
+      before { get new_patient_order_path(patient_id) }
+      it { should redirect_to(login_path) }
+    end
+
+    context 'GET /orders/:id/edit' do
+      before { get edit_order_path(order_id) }
+      it { should redirect_to(login_path) }
+    end
+
+    context 'GET /orders/:order_id/inspections' do
+      before { get order_inspections_path(order_id) }
+      it { should redirect_to(login_path) }
+    end
+
+    context 'GET /orders/:order_id/inspections/new' do
+      before { get new_order_inspection_path(order_id) }
+      it { should redirect_to(login_path) }
+    end
+
+    context 'GET /inspections/:id/edit' do
+      before { get edit_inspection_path(inspection_id) }
+      it { should redirect_to(login_path) }
+    end
+  end
 end
