20ac4d541b283c779fe5bbe40ac052cac6255262
arsley <blue20will@gmail.com>
2018-11-28 11:08:46 +0900

    fix: Define Log#to_render instead of ApplicationHelper.render_log

--- web/app/models/log.rb
+++ web/app/models/log.rb
@@ -6,4 +6,8 @@ class Log < ApplicationRecord
 
   # relation
   belongs_to :employee
+
+  def to_render
+    content.sub('__', "<a href=\"/orders/#{order_id}/inspections\">オーダー##{order_id}</a>").html_safe
+  end
 end
