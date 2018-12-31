121b3db205129e95ea17b8236b166851857e8f38
arsley <blue20will@gmail.com>
2018-12-31 14:03:37 +0900

    fix: Modify location to deploy and configs
    [ci skip]

--- web/config/deploy.rb
+++ web/config/deploy.rb
@@ -6,10 +6,10 @@ set :rbenv_ruby, File.read('.ruby-version').strip
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w[rake gem bundle ruby rails]
 
-set :application, 'ordering-dev'
+set :application, 'Ordering-system'
 set :repo_url, 'git@github.com:arsley/Ordering-system.git'
 set :repo_tree, 'web'
-set :deploy_to, '/www/ordering-dev'
+set :deploy_to, '/www/dev.arsley.work/Ordering-system'
 set :branch, ENV['BRANCH_NAME'] || 'production'
 
 set :migration_role, :web
@@ -31,6 +31,13 @@ namespace :deploy do
   after :finished, :generate_secrets do
     on roles(:web) do
       execute "cd #{release_path} && echo -e \"production:\\n  secret_key_base: `#{fetch :rbenv_prefix} bundle exec rails secret`\" > config/secrets.yml"
+    end
+  end
+
+  desc 'Generate link for sub URI'
+  after :finished, :generate_link do
+    on roles(:web) do
+      execute "ln -s #{fetch(:deploy_to)}/current/public /www/dev.arsley.work/ordering"
     end
   end
 
