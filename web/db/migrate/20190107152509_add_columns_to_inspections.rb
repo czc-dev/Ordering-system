121b8dda164dafe2a9f529dba67d2019bb61e614
arsley <blue20will@gmail.com>
2019-01-17 13:29:21 +0900

    add: Add column to inspections by updating migration

--- web/db/migrate/20190107152509_add_columns_to_inspections.rb
+++ web/db/migrate/20190107152509_add_columns_to_inspections.rb
@@ -2,5 +2,6 @@ class AddColumnsToInspections < ActiveRecord::Migration[5.2]
   def change
     add_column :inspections, :sample, :string
     add_column :inspections, :result, :string
+    add_column :inspections, :booked_at, :datetime
   end
 end
