11777890b818f7c7fcdd3387194f389189007438
arsley <blue20will@gmail.com>
2019-03-27 02:34:30 +0900

    add: Install StimulusJS with turbolinks

--- app/bundles/javascripts/entries/application.js
+++ app/bundles/javascripts/entries/application.js
@@ -1,3 +1,12 @@
 import '@style/entries/application.scss';
 // remote: true使う場合コメント外してください。
 // import "rails-ujs";
+
+require('turbolinks').start();
+
+import { Application } from 'stimulus';
+import { definitionsFromContext } from 'stimulus/webpack-helpers';
+
+const application = Application.start();
+const context = require.context('@js/controllers', true, /\.js$/);
+application.load(definitionsFromContext(context));
