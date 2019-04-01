204465475d5bcdc712ade6a3685ee5866745a4b0
arsley <blue20will@gmail.com>
2019-04-01 11:45:21 +0900

    fix/rm: Unify 2 controllers and modify related views
    modal_controller and selected_inspection_controller were same so
    unified.

--- app/bundles/javascripts/controllers/components/removebtn_controller.js
+++ app/bundles/javascripts/controllers/components/removebtn_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  remove() {
+  click() {
     this.element.remove();
   }
 }
--- app/bundles/javascripts/controllers/components/selected_inspection_controller.js
+++
@@ -1,7 +0,0 @@
-import { Controller } from 'stimulus';
-
-export default class extends Controller {
-  remove() {
-    this.element.remove();
-  }
-}
