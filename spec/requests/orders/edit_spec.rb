0d94388d5b45de9309593f6eea32bc3b1fd9a594
arsley <blue20will@gmail.com>
2019-08-13 16:03:01 +0900

    :zap: Add necessary 'require' statement

--- spec/requests/orders/edit_spec.rb
+++ spec/requests/orders/edit_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'rails_helper'
+
 RSpec.describe 'Orders GET /orders/:id/edit', type: :request, js: true do
   pending 'Use /ajax/orders/:id/edit to GET modal'
 end
