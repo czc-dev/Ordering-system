acee834abe65eb6431913a965dcd3d263c2727ed
arsley <blue20will@gmail.com>
2020-04-11 15:09:10 +0900

    :heavy_plus_sign: Configure eslint

--- app/webpacker/javascripts/entry/application.js
+++ app/webpacker/javascripts/entry/application.js
@@ -1,10 +1,10 @@
-/* eslint no-undef:0 */
+/* eslint import/no-unresolved: 0 */
+
 import '@style/entry/application.scss';
-
-require('turbolinks').start();
-
 import { Application } from 'stimulus';
 import { definitionsFromContext } from 'stimulus/webpack-helpers';
+
+require('turbolinks').start();
 
 const application = Application.start();
 const context = require.context('@js/controllers', true, /\.js$/);
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,3 @@
-/* eslint no-undef: 0 */
 const { environment } = require('@rails/webpacker');
 
 const resolve = require('./partial/resolve');
