ee7057c4fad53abdf258c4a83973372edba051ae
Aoshi Fujioka <blue20will@gmail.com>
2020-05-09 21:09:40 +0900

    :zap: Improve spec more strictly for orders#create and exams#create

--- spec/requests/exams/create_spec.rb
+++ spec/requests/exams/create_spec.rb
@@ -2,35 +2,34 @@
 
 RSpec.describe 'Exams POST /orders/:order_id/exams', type: :request, js: true do
   let(:employee) { create(:employee) }
-  let(:patient) { create(:patient) }
-  let(:order) { patient.orders.first }
-  let(:exam) { order.exams.first }
+  let(:order) { create(:order) }
 
   # 全てのアクションにおいてログインが必要です
   before { post login_path, params: { username: employee.username, password: employee.password } }
 
+  subject { post order_exams_path(order.id), params: params }
+
   context 'when the request is valid' do
-    let(:valid_params) do
+    let(:params) do
       { order: { exam_item_ids: (1..10).to_a } }
     end
-    before { post order_exams_path(order.id), params: valid_params }
 
-    it 'creates a order' do
-      expect(assigns[:order]).to eq(Order.last)
+    it 'adds exams to order as you selected' do
+      expect { subject }.to change { order.exams.count }.by(10)
     end
 
-    it { should redirect_to(order_exams_path(Order.last)) }
+    it { is_expected.to redirect_to(order_exams_path(order.id)) }
   end
 
   context 'when no exams selected' do
-    let(:invalid_params) do
+    let(:params) do
       { order: { empty: '' } }
     end
-    before { post order_exams_path(order.id), params: invalid_params }
 
-    it { should render_template('new') }
+    it { is_expected.to render_template('new') }
 
     it 'returns status code 400(Bad request)' do
+      subject
       expect(response).to have_http_status(400)
     end
   end
--- spec/requests/orders/create_spec.rb
+++ spec/requests/orders/create_spec.rb
@@ -4,34 +4,38 @@ RSpec.describe 'Orders POST /patients/:patient_id/orders', type: :request, js: t
   # モックとして作成される各患者は
   # 10個の検査からなる1つのオーダーを持ちます
   let(:patient) { create(:patient) }
-  let(:order) { patient.orders.first }
   let(:employee) { create(:employee) }
+
+  subject { post patient_orders_path(patient.id), params: params }
 
   # 全てのアクションにおいてログインが必要です
   before { post login_path, params: { username: employee.username, password: employee.password } }
 
   context 'when the request is valid' do
-    let(:valid_params) do
+    let(:params) do
       { order: { exam_item_ids: (1..10).to_a, may_result_at: Time.zone.now + 10.days } }
     end
-    before { post patient_orders_path(patient.id), params: valid_params }
 
     it 'creates a order' do
-      expect(assigns[:order]).to eq(Order.last)
+      expect { subject }.to change { patient.orders.count }.by(1)
     end
 
-    it { should redirect_to(order_exams_path(Order.last)) }
+    it 'creates exams as you selected' do
+      expect { subject }.to change { patient.orders.first&.exams&.count || 0 }.by(10)
+    end
+
+    it { is_expected.to redirect_to(order_exams_path(patient.orders.first)) }
   end
 
   context 'when no exams selected' do
-    let(:invalid_params) do
+    let(:params) do
       { order: { may_result_at: Time.zone.now + 10.days } }
     end
-    before { post patient_orders_path(patient.id), params: invalid_params }
 
-    it { should render_template('new') }
+    it { is_expected.to render_template('new') }
 
     it 'returns status code 400(Bad request)' do
+      subject
       expect(response).to have_http_status(400)
     end
   end
