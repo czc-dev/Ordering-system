7b2ad5e4a4d63b54ed88c80138f33d126b85963e
Aoshi Fujioka <blue20will@gmail.com>
2020-05-10 18:29:38 +0900

    :bug::hammer: Modify params on PaperTrail::Versions#show spec and rewrite to subject instead of before

--- spec/requests/histories/index_spec.rb
+++ spec/requests/histories/index_spec.rb
@@ -6,19 +6,22 @@ RSpec.describe 'Histories (PaperTrail::Version) GET /histories', type: :request,
   # 全てのアクションにおいてログインが必要です
   before do
     post login_path, params: { username: employee.username, password: employee.password }
-    create(:exam)
   end
 
-  # TODO: 履歴件数が10件以上になるように factory を書き直す（とより厳密なテストになる）
-  before { get histories_path }
+  subject do
+    create_list(:order, 10)
+    get histories_path
+  end
 
   with_versioning do
     it 'can list up recent 10 histories of Order' do
+      subject
       expectation = PaperTrail::Version.where(item_type: 'Order').order(created_at: :desc).first(10)
       expect(assigns[:orders]).to eq(expectation)
     end
 
     it 'can list up recent 10 histories of Exam' do
+      subject
       expectation = PaperTrail::Version.where(item_type: 'Exam').order(created_at: :desc).first(10)
       expect(assigns[:exams]).to eq(expectation)
     end
--- spec/requests/histories/show_spec.rb
+++ spec/requests/histories/show_spec.rb
@@ -2,57 +2,60 @@
 
 RSpec.describe 'Histories (PaperTrail::Version) GET /histories/:id', type: :request, js: true do
   let(:employee) { create(:employee) }
-  let(:patient) { order.patient }
-  let(:order) { create(:order) }
+  let(:patient) { create(:patient) }
   let(:create_params) do
-    { order: { exams: (1..10).to_a, may_result_at: Time.zone.now + 10.days } }
+    { order: { exam_item_ids: (1..10).to_a, may_result_at: Time.zone.now + 10.days } }
   end
 
   # 全てのアクションにおいてログインが必要です
   before { post login_path, params: { username: employee.username, password: employee.password } }
 
   context 'when history has only 1 version (after created)' do
-    before do
+    subject do
       post patient_orders_path(patient.id), params: create_params
       get history_path(PaperTrail::Version.last)
     end
 
     with_versioning do
       it 'cannot show object\'s difference; it can only show "create" and "current" message' do
+        subject
         expect(assigns[:history]&.previous).to be_nil
         expect(assigns[:history]&.next).to     be_nil
       end
 
       it 'can show employee that changed this resource' do
+        subject
         expect(assigns[:version_author]).to eq(employee)
       end
     end
   end
 
   context 'when history has previous version (e.g. create → update)' do
-    let(:update_params) { { order: { canceled: true } } }
-
-    before do
+    subject do
       post patient_orders_path(patient.id), params: create_params
-      put order_path(order.id), params: update_params
+      order = patient.orders.first
+      put order_path(order.id), params: { order: { canceled: true } }
       get history_path(PaperTrail::Version.last)
     end
 
     with_versioning do
       it 'can show previous state and "current" message' do
+        subject
         expect(assigns[:history]&.previous).not_to be_nil
         expect(assigns[:history]&.next).to be_nil
       end
 
       it 'can show employee that changed this resource' do
+        subject
         expect(assigns[:version_author]).to eq(employee)
       end
     end
   end
 
   context 'when history has previous and next version (e.g. create → update → update)' do
-    before do
+    subject do
       post patient_orders_path(patient.id), params: create_params
+      order = patient.orders.first
       put order_path(order.id), params: { order: { canceled: true } }
       put order_path(order.id), params: { order: { canceled: false } }
       get history_path(PaperTrail::Version.order(created_at: :desc).second)
@@ -60,11 +63,13 @@ RSpec.describe 'Histories (PaperTrail::Version) GET /histories/:id', type: :requ
 
     with_versioning do
       it 'can show previous and next state of specific object' do
+        subject
         expect(assigns[:history]&.previous).not_to be_nil
         expect(assigns[:history]&.next).not_to be_nil
       end
 
       it 'can show employee that changed this resource' do
+        subject
         expect(assigns[:version_author]).to eq(employee)
       end
     end
