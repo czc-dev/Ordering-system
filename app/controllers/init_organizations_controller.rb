ee14e67beddd50ddd500c26fb0f6d5639ae505c5
Aoshi Fujioka <blue20will@gmail.com>
2020-05-16 19:27:50 +0900

    :zap::x: Implement InitOrganizationsController
    Fixing link_to path is ignored in this time.

--- app/controllers/init_organizations_controller.rb
+++ app/controllers/init_organizations_controller.rb
@@ -1,7 +1,17 @@
 class InitOrganizationsController < ApplicationController
-  def new
-  end
+  skip_before_action :require_login
+
+  def new; end
 
   def create
+    if params[:email].present? && params[:email].match?(Employee::EMAIL_REGEX)
+      Invitation.create!(email: params[:email])
+      # send_mail_new_organization
+      flash[:success] = 'メールを送信します。確認してください。'
+      render :new, status: :ok
+    else
+      flash[:warning] = 'メールアドレスを正しく入力してください。'
+      render :new, status: :bad_request
+    end
   end
 end
