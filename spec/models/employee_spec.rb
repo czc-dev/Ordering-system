49f63297872415335a85dc5a9e9d3e6acb466b39
Aoshi Fujioka <blue20will@gmail.com>
2020-05-16 15:57:21 +0900

    :bug: Fix model spec on Employee to be able to verify uniquenss validation

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,7 +1,9 @@
 RSpec.describe Employee, type: :model do
+  # create employee to verify uniqueness validation
+  let!(:employee) { create(:employee) }
+
   # validation
   it { should validate_presence_of(:fullname) }
-  it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
 
   # values
